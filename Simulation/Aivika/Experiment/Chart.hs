cb9e5728a511486bdf9634eff0a6b5cd4e498d2c
David Sorokin <david.sorokin@gmail.com>
2014-02-01 13:12:12 +0400

    Returning to the BSD3 license.

--- Simulation/Aivika/Experiment/Chart.hs
+++ Simulation/Aivika/Experiment/Chart.hs
@@ -2,7 +2,7 @@
 -- |
 -- Module     : Simulation.Aivika.Experiment.Chart
 -- Copyright  : Copyright (c) 2012, David Sorokin <david.sorokin@gmail.com>
--- License    : GPL
+-- License    : BSD3
 -- Maintainer : David Sorokin <david.sorokin@gmail.com>
 -- Stability  : experimental
 -- Tested with: GHC 7.6.3
