04677d0e1b5ff837cd626584815c6e5e6af1a6da
David Sorokin <david.sorokin@gmail.com>
2015-06-28 08:28:05 +0300

    Updated example.

--- examples/Furnace/Experiment.hs
+++ examples/Furnace/Experiment.hs
@@ -8,6 +8,8 @@ import Simulation.Aivika
 import Simulation.Aivika.Experiment
 import Simulation.Aivika.Experiment.Chart
 
+import qualified Simulation.Aivika.Results.Transform as T
+
 import Model
 
 -- | The simulation specs.
@@ -27,6 +29,18 @@ experiment =
     experimentRunCount = 100,
     experimentTitle = "The Furnace model (the Monte-Carlo simulation)" }
 
+inputIngotCount  = resultByName inputIngotCountName
+loadedIngotCount = resultByName loadedIngotCountName
+outputIngotCount = resultByName outputIngotCountName
+pitCount         = resultByName pitCountName
+heatingTime      = resultByName heatingTimeName
+outputIngotTemp  = resultByName outputIngotTempName
+
+furnaceQueue     = T.Queue $ resultByName furnaceQueueName
+furnaceQueueCountStats = T.tr $ T.queueCountStats furnaceQueue
+furnaceQueueWaitTime   = T.tr $ T.queueWaitTime furnaceQueue
+furnaceQueueRate       = T.tr $ T.queueRate furnaceQueue
+
 generators :: ChartRendering r => [WebPageGenerator r]
 generators =
   [outputView defaultExperimentSpecsView,
@@ -35,97 +49,77 @@ generators =
      deviationChartTitle = "Deviation Chart - 1",
      deviationChartPlotTitle = "The input, loaded and output ingot counts",
      deviationChartRightYSeries =
-       resultByName inputIngotCountName <>
-       resultByName loadedIngotCountName <>
-       resultByName outputIngotCountName },
+       inputIngotCount <> loadedIngotCount <> outputIngotCount },
    outputView $ defaultFinalHistogramView {
      finalHistogramTitle = "Final Histogram - 1",
      finalHistogramPlotTitle = "The distribution of input, loaded and output " ++
                                "ingot counts in the final time point.",
      finalHistogramSeries =
-       resultByName inputIngotCountName <>
-       resultByName loadedIngotCountName <>
-       resultByName outputIngotCountName },
+       inputIngotCount <> loadedIngotCount <> outputIngotCount },
    outputView $ defaultFinalStatsView {
      finalStatsTitle = "Final Statistics - 1",
      finalStatsDescription = "The summary of input, loaded and output " ++
                              "ingot counts in the final time point.",
      finalStatsSeries =
-       resultByName inputIngotCountName <>
-       resultByName loadedIngotCountName <>
-       resultByName outputIngotCountName },
+       inputIngotCount <> loadedIngotCount <> outputIngotCount },
    outputView $ defaultDeviationChartView {
      deviationChartTitle = "Deviation Chart - 2",
      deviationChartPlotTitle = "The used pit count",
-     deviationChartRightYSeries =
-       resultByName pitCountName },
+     deviationChartRightYSeries = pitCount },
    outputView $ defaultFinalHistogramView {
      finalHistogramTitle = "Final Histogram - 2",
      finalHistogramPlotTitle = "The used pit count in the final time point.",
-     finalHistogramSeries =
-       resultByName pitCountName },
+     finalHistogramSeries = pitCount },
    outputView $ defaultFinalStatsView {
      finalStatsTitle = "Final Statistics - 2",
      finalStatsDescription = "The summary of the used pit count in the final time point.",
-     finalStatsSeries =
-       resultByName pitCountName },
+     finalStatsSeries = pitCount },
    outputView $ defaultDeviationChartView {
      deviationChartTitle = "Deviation Chart - 3",
      deviationChartPlotTitle = "The average queue size",
-     deviationChartRightYSeries =
-       resultByName furnaceQueueName >>> resultById QueueCountStatsId },
+     deviationChartRightYSeries = furnaceQueueCountStats },
    outputView $ defaultFinalStatsView {
      finalStatsTitle = "Final Statistics - 3",
      finalStatsDescription = "The summary of the average queue size in the final time point.",
-     finalStatsSeries =
-       resultByName furnaceQueueName >>> resultById QueueCountStatsId },
+     finalStatsSeries = furnaceQueueCountStats },
    outputView $ defaultDeviationChartView {
      deviationChartTitle = "Deviation Chart - 4",
      deviationChartPlotTitle = "The mean wait time",
-     deviationChartRightYSeries =
-       resultByName furnaceQueueName >>> resultById QueueWaitTimeId },
+     deviationChartRightYSeries = furnaceQueueWaitTime },
    outputView $ defaultFinalStatsView {
      finalStatsTitle = "Final Statistics - 4",
      finalStatsDescription = "The summary of the mean wait time in " ++
                              "the final time point.",
-     finalStatsSeries =
-       resultByName furnaceQueueName >>> resultById QueueWaitTimeId },
+     finalStatsSeries = furnaceQueueWaitTime },
    outputView $ defaultDeviationChartView {
      deviationChartTitle = "Deviation Chart - 5",
      deviationChartPlotTitle = "The queue rate",
-     deviationChartRightYSeries =
-       resultByName furnaceQueueName >>> resultById QueueRateId },
+     deviationChartRightYSeries = furnaceQueueRate },
    outputView $ defaultFinalStatsView {
      finalStatsTitle = "Final Statistics - 5",
      finalStatsDescription = "The summary of the queue rate in " ++
                              "the final time point.",
-     finalStatsSeries =
-       resultByName furnaceQueueName >>> resultById QueueRateId },
+     finalStatsSeries = furnaceQueueRate },
    outputView $ defaultDeviationChartView {
      deviationChartTitle = "Deviation Chart - 6",
      deviationChartPlotTitle = "The mean heating time",
-     deviationChartRightYSeries =
-       resultByName heatingTimeName },
+     deviationChartRightYSeries = heatingTime },
    outputView $ defaultFinalStatsView {
      finalStatsTitle = "Final Statistics - 6",
      finalStatsDescription = "The summary of the mean heating time in " ++
                              "the final time point.",
-     finalStatsSeries =
-       resultByName heatingTimeName },
+     finalStatsSeries = heatingTime },
    outputView $ defaultDeviationChartView {
      deviationChartTitle = "Deviation Chart - 7",
      deviationChartPlotTitle = "The output ingot temperature",
-     deviationChartRightYSeries =
-       resultByName outputIngotTempName },
+     deviationChartRightYSeries = outputIngotTemp },
    outputView $ defaultFinalHistogramView {
      finalHistogramTitle = "Final Histogram - 7",
      finalHistogramPlotTitle = "The output ingot temperature in " ++
                                "the final time point.",
-     finalHistogramSeries =
-       resultByName outputIngotTempName },
+     finalHistogramSeries = outputIngotTemp },
    outputView $ defaultFinalStatsView {
      finalStatsTitle = "Final Statistics - 7",
      finalStatsDescription = "The summary of the output ingot temperature in " ++
                              "the final time point.",
-     finalStatsSeries =
-       resultByName outputIngotTempName } ]
+     finalStatsSeries = outputIngotTemp } ]
